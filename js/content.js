9ea15670940872c4fe321fbe1f8ebc139f97f8cf
aaa <aaa@email.com>
2018-11-22 17:58:39 +0800

    公告

--- js/content.js
+++ js/content.js
@@ -8,7 +8,7 @@ window.hot = [
 ];
 
 window.announcement = [
-	["重要更新", "2018-11-21", "coToken安卓版新版发布，强烈推荐<a href=\"https://hifus.github.io/coToken_v0.2.4.apk\">下载更新</a>！", true],
+    ["重要公告", "2018-11-22", "有关我们的工作方向和大家关心的若干问题，请<a href=\"https://etherscan.io/address/0x75cb775f1a0f6e3718aeeb2435ea010c6d8ec0b5#code\">查看公告</a>", true],
+    ["重要更新", "2018-11-21", "coToken安卓版新版发布，强烈推荐<a href=\"https://hifus.github.io/coToken_v0.2.4.apk\">下载更新</a>！", true],
     ["准备注册", "2018-11-15", "今晚8点开启<a href=\"https://hifus.github.io/a.html\" style=\"text-decoration: underline;\">层级注册通道</a>，请领导人向自己的家人通告自己的以太坊账号，请家人们认清自己的领导人账号，进行注册！未能找到领导人的家人可随意选择页面内的一级推荐人账号作为上级。(一级推荐人列表只展示两天)<b style=\"color:#ff6c00;\">各级领导人请尽快注册，未注册的账号是不能发展下级的！</b>", true],
-    ["特级会员激活状态", "2018-11-15", "很感谢家人们的关怀和支持，特级会员邀请工作顺利完成！由于有位家人贡献突出，所以增加了一个特级会员名额，以表感谢！目前发出邀请码17个，15个已激活，1个被取消，1个未激活。无特殊原因不再增加名额。", true],
 ];
