9086dd7794b9391b14518d706e9874b9165967fa
aaa <aaa@email.com>
2018-12-03 01:51:04 +0800

    启航

--- js/content.js
+++ js/content.js
@@ -9,8 +9,8 @@ window.hot = [
 ];
 
 window.announcement = [
+    ["LW2成功启航", "2018-12-3", "很高兴LW2成功启航了！这艘搭载着我们梦想的大船终将驶向何方，就要靠大家一起努力一起掌舵了！请参见我们的<a href=\"https://etherscan.io/address/0xfb68b2f6538f549dc2fb3c8bd2354bc1061b7874#code\">新公告</a>，一起奋斗吧！", true],
     ["LW2上线啦", "2018-11-29", "大家期待已久的LW2将在2018-11-30 16:00正式启动！全新规则，全新玩法，刺激多多，分红多多，合力打造以太坊游戏新标杆！<br>详细规则参见游戏页面中的说明。", true],
     ["重要公告", "2018-11-22", "有关我们的工作方向和大家关心的若干问题，请<a href=\"https://etherscan.io/address/0x75cb775f1a0f6e3718aeeb2435ea010c6d8ec0b5#code\">查看公告</a>", true],
     ["重要更新", "2018-11-21", "coToken安卓版新版发布，强烈推荐<a href=\"https://hifus.github.io/coToken_v0.2.4.apk\">下载更新</a>！", true],
-    ["准备注册", "2018-11-15", "今晚8点开启<a href=\"https://hifus.github.io/a.html\" style=\"text-decoration: underline;\">层级注册通道</a>，请领导人向自己的家人通告自己的以太坊账号，请家人们认清自己的领导人账号，进行注册！未能找到领导人的家人可随意选择页面内的一级推荐人账号作为上级。(一级推荐人列表只展示两天)<b style=\"color:#ff6c00;\">各级领导人请尽快注册，未注册的账号是不能发展下级的！</b>", true],
 ];
