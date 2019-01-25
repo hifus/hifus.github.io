684cf3e5b9f195f17c884fb732a0ef137759a4c6
aaa <aaa@email.com>
2019-01-25 18:28:26 +0800

    日期有误

--- js/content.js
+++ js/content.js
@@ -10,7 +10,7 @@ window.hot = [
 ];
 
 window.announcement = [
-	["LW2又被堵了","2018-1-23","LW2第四轮开奖，是堵塞区块的结果，请查看<a href=\"https://etherscan.io/address/0x0b76544f6c413a555f309bf76260d1e02377c02a\">此合约</a>或<a href=\"https://hifus.github.io/images/lw4.png\">截图</a>，开奖之后，停止了堵塞。为此，我们决定拒绝发奖！",true],
+	["LW2又被堵了","2019-1-23","LW2第四轮开奖，是堵塞区块的结果，请查看<a href=\"https://etherscan.io/address/0x0b76544f6c413a555f309bf76260d1e02377c02a\">此合约</a>或<a href=\"https://hifus.github.io/images/lw4.png\">截图</a>，开奖之后，停止了堵塞。为此，我们决定拒绝发奖！",true],
 	["FUS再次分红","2019-1-16","中午12点，我们成功进行了FUS第二次分红，总计分红<b>654 ETH</b>！其中336ETH来自LW2，318ETH来自Biigo（包括旧Biigo的123ETH），可参见<a href=\"https://etherscan.io/tx/0x14564abe23bce132da9c0c7b05770d6840bcaf9e49fc81f84c3ac307e1a46e91\">这次</a>和<a href=\"https://etherscan.io/tx/0x9573225b8806698946319866c86cfc8e55d68d7f0c5fb38b86ef1aeb3bde38f2\">这次</a>交易。同时旧Biigo合约正式作废！",true],
 	["LW2开门红！！！","2019-1-13","中了<b>569ETH</b>！！！我们的LW2又开奖了，再次证明不懈的坚守最终会得到丰厚的回报！他的<a href=\"https://etherscan.io/tx/0xda4db3d8061fe66ca57b75a33fc145c83c8f3fa49f9a46c920a2faceb647e53b\">幸运一投</a>依然是0.002ETH，在7056171到7056188块过程中，大家都放弃了，于是幸运之神选择了他！<a href=\"https://etherscan.io/block/7056189\">7056189</a>块用了183秒才挖出，当大家猛然醒来时已经与幸运之神擦肩而过了！恭喜他！愿他今年依然幸运长伴！", true],
 	["FUS首次分红","2019-1-1","此刻，我们迎来了新的一年，首先祝福大家新年进步、平安发财！在这万众同乐的日子，我们的FUS也会在中午12:00进行首次分红，本次分红的总数近1600ETH！！从本次分红起，以后每15天进行一次分红！除了用于分红外，基于FUS的更有价值的应用，也将在以后出现！2019年，是家人们和FUS腾飞之年！"],
