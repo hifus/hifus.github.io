1531a04e45110a6c9e6c063ba5861a681b25dabf
aaa <aaa@email.com>
2019-02-10 16:10:49 +0800

    加上中奖投注

--- js/content.js
+++ js/content.js
@@ -10,7 +10,7 @@ window.hot = [
 ];
 
 window.announcement = [
-	["新春爆三奖","2019-2-9","在这个新春假期，我们的LW2迎来了三位赢家，都是合法开奖！他们总计收获了1065ETH，恭喜他们！！！各位赢家不断地证明了坚持才会胜利这个不变真理！由于我们的技术团队在放假前设置的开奖功能没有起效，导致众多猜测，在此我们深表歉意。幸好现在币价上扬，没有给赢家们造成损失。再次感谢大家的坚守！祝大家新年旺旺旺！！"],
+	["新春爆三奖","2019-2-9","在这个新春假期，我们的LW2迎来了三位赢家，都是合法开奖（三次投注分别是<a href=\"https://etherscan.io/tx/0x15b324f878cae9c1d5054ca033ed4f40f5d6b762ea9e074c962268f54bb226cf\">第5轮</a>、<a href=\"https://etherscan.io/tx/0x89149d52a5751ecd7448f20a29312a5e20592cae469bbb98c282a557db94fc33\">第6轮</a>、<a href=\"https://etherscan.io/tx/0xb7127bf03a6fada7ecf9c17c480dc33a729966324ca0ad32eb589e7dd2551052\">第7轮</a>）！他们总计收获了<b>1065 ETH</b>，恭喜他们！！！各位赢家不断地证明了坚持才会胜利这个不变真理！由于我们的技术团队在放假前设置的开奖功能没有起效，导致众多猜测，在此我们深表歉意。幸好现在币价上扬，没有给赢家们造成损失。再次感谢大家的坚守！祝大家新年旺旺旺！！", true],
 	["LW2又被堵了","2019-1-23","LW2第四轮开奖，是堵塞区块的结果，请查看<a href=\"https://etherscan.io/address/0x0b76544f6c413a555f309bf76260d1e02377c02a\">此合约</a>或<a href=\"https://hifus.github.io/images/lw4.png\">截图</a>，开奖之后，停止了堵塞。为此，我们决定拒绝发奖！",true],
 	["FUS再次分红","2019-1-16","中午12点，我们成功进行了FUS第二次分红，总计分红<b>654 ETH</b>！其中336ETH来自LW2，318ETH来自Biigo（包括旧Biigo的123ETH），可参见<a href=\"https://etherscan.io/tx/0x14564abe23bce132da9c0c7b05770d6840bcaf9e49fc81f84c3ac307e1a46e91\">这次</a>和<a href=\"https://etherscan.io/tx/0x9573225b8806698946319866c86cfc8e55d68d7f0c5fb38b86ef1aeb3bde38f2\">这次</a>交易。同时旧Biigo合约正式作废！",true],
 	["LW2开门红！！！","2019-1-13","中了<b>569ETH</b>！！！我们的LW2又开奖了，再次证明不懈的坚守最终会得到丰厚的回报！他的<a href=\"https://etherscan.io/tx/0xda4db3d8061fe66ca57b75a33fc145c83c8f3fa49f9a46c920a2faceb647e53b\">幸运一投</a>依然是0.002ETH，在7056171到7056188块过程中，大家都放弃了，于是幸运之神选择了他！<a href=\"https://etherscan.io/block/7056189\">7056189</a>块用了183秒才挖出，当大家猛然醒来时已经与幸运之神擦肩而过了！恭喜他！愿他今年依然幸运长伴！", true],
