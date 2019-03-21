f278aae3787cdfae91ccb4c8b9699f7ebe9d3c5e
aaa <aaa@email.com>
2019-03-21 11:06:29 +0800

    LW24

--- js/content.js
+++ js/content.js
@@ -12,6 +12,7 @@ window.hot = [
 
 window.announcement = [
 	["新阶段新应用","2019-3-21","我们的新应用“幸运推币”上线了，标志着我们的平台进入一个全新的阶段！在这个阶段中，FUS就是主角！详情请查看<a href=\"https://etherscan.io/address/0xf0fea813af6f29d954dba90c37bf0906b6de8390#code\">给家人们的信</a>！这个新应用，就是第一个展示应用，也是第一个全民分红的应用！快去告诉更多人吧！", true],
+	["LW2第34轮开奖","2019-3-21","LW2第34轮开奖了！<a href=\"https://etherscan.io/tx/0x87d0fc71953d4b2829e206ba71ce27b2408a9cf1c3280de8b7f39ddb16fe3292\">最后购买种子</a>执行在<a href=\"https://etherscan.io/block/7407888\">7407888</a>块。执行完这个交易后，结束时间被推迟到几乎2分钟之后。但出乎意料的是，<a href=\"https://etherscan.io/block/7407889\">7407889</a>块竟然耗时130秒才被算出，谁也无力回天了！", true],
 	["LW2第29轮开奖","2019-3-14","LW2第29轮开奖了！<a href=\"https://etherscan.io/tx/0x636984acca9782e64099ea06b60e15b4aec140b04dad7e0b52660aba78a051d4\">最后购买种子</a>执行在<a href=\"https://etherscan.io/block/7365042\">7365042</a>块，这一块用了92秒才算出，并且刚好有大量Gas价格大于20G的正常请求等待处理。因此，这块只包含了1个购买交易，这个交易的价格是59.9G。而绝大部分购买请求的Gas价格在20G以下，从而没有机会执行。等到45秒后执行<a href=\"https://etherscan.io/block/7365043\">7365043</a>块中的请求时，倒数已经结束了！再到了7365044块，才开始处理Gas价格为20G及以下的购买请求。因此，<span style=\"color: red;\">关键时刻，Gas价格要高，譬如至少30G，才能夺得先机</span>！", true],
 	["LW2第27轮开奖","2019-3-9","LW2第27轮开奖了，原因是碰到一个不含任何交易的块：7331865块！<a href=\"https://etherscan.io/tx/0x5773081579df71f6fbd695a842793abdf9fb1cdcbbf5d1b92e5a84becf05a8cc\">最后购买种子</a>执行在7331864块，下一块<a href=\"https://etherscan.io/block/7331865\">7331865</a>用了65秒才算出，并且它不含任何交易（这是允许出现的）。等到20秒后执行<a href=\"https://etherscan.io/block/7331866\">7331866</a>块中的请求时，倒数已经结束了！<span style=\"color: red;\">所以，提前1、2分钟购买种子很重要，幸运一次就足够翻本了！</span>", true],
 ];
