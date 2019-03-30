c2dfcc5243cf347a602f63ea6ecd1e70c4a177d2
aaa <aaa@email.com>
2019-03-30 16:11:01 +0800

    新登记2

--- js/items.js
+++ js/items.js
@@ -1,6 +1,6 @@
 window.items = [
     ["LW2(旧)", "LastWinner2", false, "lastwinner2.html"],
-    ["旧LW2登记", "LastWinner2Fus", false, "lw2fus.html"],
+    ["旧LW2登记", "LastWinner2Fus2", false, "lw2fus.html"],
     ["LW2(新)", "LastWinner2b", false, "lastwinner2b.html"],
     ["BG矿场", "Biigo2", false, "biigo2.html"],
     ["推币玩家", "CoinPusher", false, "coinpusher.html"],
