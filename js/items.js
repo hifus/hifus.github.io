01822d9183b9e33e0c704c3ef6b2f05877e9d5e8
aaa <aaa@email.com>
2019-04-01 17:59:07 +0800

    无需登记

--- js/items.js
+++ js/items.js
@@ -1,7 +1,5 @@
 window.items = [
-    ["LW2(旧)", "LastWinner2", false, "lastwinner2.html"],
-    ["旧LW2登记", "LastWinner2Fus2", false, "lw2fus.html"],
-    ["LW2(新)", "LastWinner2b", false, "lastwinner2b.html"],
+    ["LW2", "LastWinner2b", false, "lastwinner2b.html"],
     ["BG矿场", "Biigo2", false, "biigo2.html"],
     ["推币玩家", "CoinPusher", false, "coinpusher.html"],
     ["推币全民", "CoinPusherTwin", true, "coinpusher.html"],
