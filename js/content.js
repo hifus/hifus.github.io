13a251a514423379e9054f9f2710de58dd27a729
aaa <aaa@email.com>
2019-04-26 01:32:25 +0800

    lw2f

--- js/content.js
+++ js/content.js
@@ -1,5 +1,5 @@
 window.hot = [
-    ["lastwinner2e.html", "images/lw2.png", "LastWinner2"],
+    ["lastwinner2f.html", "images/lw2.png", "LastWinner2"],
     ["biigo2.html", "images/bg.png", "Biigo矿场"],
     ["coinpusher.html", "images/coinpusher.png", "幸运推币"],
     ["a.html", "images/affiliate.jpg", "加入层级"],
@@ -8,12 +8,13 @@ window.hot = [
     ["minister.html", "images/special.png", "特级会员"],
     ["lastwinner2.html", "images/lw2.png", "LastWinner2(旧)"],
     ["lastwinner2b.html", "images/lw2.png", "LastWinner2(旧)"],
+    ["lastwinner2e.html", "images/lw2.png", "LastWinner2(旧)"],
     ["metamask.html", "images/metamask-logo.png", "MetaMask"],
     ["https://etherscan.io/", "images/EtherscanLogo.png", "Etherscan"],
 ];
 
 window.announcement = [
-	["LW2新版上线","2019-4-18","LW2新版在18日晚上8点重新上线。新版LW2增加了防探测功能，游戏更加公平。并且将每节开头的准备时间延长为10分钟，每轮开头的准备时间延长为20分钟，让大家能有更充分的准备！快去告诉更多人吧！", true],
+	["LW2新版上线","2019-4-26","LW2新版在26日中午12点重新上线。新版LW2中，抢奖和护奖所获得的FUS数量有差别，每投入0.1ETH抢奖，将获得5 FUS，而每投入0.1ETH护奖，将获得13 FUS。向该游戏当前版本抢奖或护奖累计投入0.1ETH，则可获得该游戏的分红资格。若不再向当前版本投入，但在以前版本中投入累计0.1ETH以上的玩家，也可在激活页面放出后，通过激活页面获得分红资格。", true],
 	["LW2再迎赢家","2019-4-10","LW2第3轮结束了，赢家收获了<b>328 ETH</b>的奖励，按照当前的价格，接近<b>59000美金</b>。此轮<a href=\"https://etherscan.io/tx/0xf6389ad159f2d2b29ffd2e38d5029698bdb2dc6723c7399abb8d752d8fe687ba\">最后抢奖</a>在7536167块。此抢奖请求执行后，离倒计时结束还有65秒。但<a href=\"https://etherscan.io/block/7536168\">第7536168块</a>用了78秒才挖出，那时倒计时已经结束了。其实若有10颗种子的护奖请求抢在所有抢奖请求前执行，就能延长20秒把奖救回了。但大家在最后时刻还在抢奖，结果倒计时小于0后执行的抢奖请求，就是帮赢家开奖了！<br>最后提示一点：护奖请求gas价格要<b>高于20G</b>（不是等于20G），才能保证执行在所有抢奖请求之前。", true],
 	["新阶段新应用","2019-3-21","我们的新应用“幸运推币”上线了，标志着我们的平台进入一个全新的阶段！在这个阶段中，FUS就是主角！详情请查看<a href=\"https://etherscan.io/address/0xf0fea813af6f29d954dba90c37bf0906b6de8390#code\">给家人们的信</a>！这个新应用，就是第一个展示应用，也是第一个全民分红的应用！快去告诉更多人吧！", true],
 ];
--- js/items.js
+++ js/items.js
@@ -1,7 +1,8 @@
 window.items = [
-    ["LW2", "LastWinner2e", false, "lastwinner2e.html"],
+    ["LW2", "LastWinner2f", false, "lastwinner2f.html"],
     ["BG矿场", "Biigo2", false, "biigo2.html"],
     ["推币玩家", "CoinPusher", false, "coinpusher.html"],
     ["推币全民", "CoinPusherTwin", true, "coinpusher.html"],
     ["LW2(旧)", "LastWinner2b", false, "lastwinner2b.html"],
+    ["LW2(旧)", "LastWinner2e", false, "lastwinner2e.html"],
 ];
