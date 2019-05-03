e4997c3fa6c2e849f465b154549b5bf20f951f7c
aaa <aaa@email.com>
2019-05-03 10:01:23 +0800

    去除旧LW

--- js/items.js
+++ js/items.js
@@ -3,6 +3,4 @@ window.items = [
     ["BG矿场", "Biigo2", false, "biigo2.html"],
     ["推币玩家", "CoinPusher", false, "coinpusher.html"],
     ["推币全民", "CoinPusherTwin", true, "coinpusher.html"],
-    ["LW2(旧)", "LastWinner2b", false, "lastwinner2b.html"],
-    ["LW2(旧)", "LastWinner2e", false, "lastwinner2e.html"],
 ];
